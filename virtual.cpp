dc9de2b30eb2f704d3a34d0e4547c3fa751ae323
refazul-refat <refazul.refat@gmail.com>
2014-10-30 12:00:16 +0600

    Create virtual.cpp

---
+++ virtual.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+class Animal
+{
+public:
+	virtual void eat()
+	{
+		cout<<"Eating Generic Food"<<endl;
+	}
+};
+class Dog : public Animal
+{
+public:
+	void eat()
+	{
+		cout<<"Eating Dog Food"<<endl;
+	}
+};
+class Cat : public Animal
+{
+public:
+	void eat()
+	{
+		cout<<"Eating Cat Food"<<endl;
+	}
+};
+
+void action(Animal *a)
+{
+	a->eat();
+}
+
+int main()
+{
+	Animal *a=new Animal();
+	Dog *d=new Dog();
+	Cat *c=new Cat();
+
+	action(a);
+	action(d);
+	action(c);
+
+	/* Existence of virtual keyword before Animal::eat() function causes
+	its subclasses to use their own eat() function [if they have one] */
+	return 0;
+}
